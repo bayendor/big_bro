fd37c32abcf1372bbc735da51dbb3494438e7a2a
Alex <96dalex@gmail.com>
2015-03-10 14:58:49 -0600

    indent runner class 1 over

--- lib/big_brother/runner.rb
+++ lib/big_brother/runner.rb
@@ -1,21 +1,19 @@
-class BigBrother
-  class Runner
+class BigBrother::Runner
 
-    def self.run(cmd_line_args, options)
-      if options[:version]
-        print_version
-      else
-        do_other_stuff
-      end
+  def self.run(cmd_line_args, options)
+    if options[:version]
+      print_version
+    else
+      do_other_stuff
     end
-
-    def self.print_version
-      puts BigBrother::VERSION
-    end
-
-    def self.do_other_stuff
-      puts "Big bro runner template"
-    end
-
   end
+
+  def self.print_version
+    puts BigBrother::VERSION
+  end
+
+  def self.do_other_stuff
+    puts "Big bro runner template"
+  end
+
 end
