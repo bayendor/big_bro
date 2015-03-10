04ce3ff9a794e1f16553c2ad05206355edab0f15
David Bayendor <dbayendor@gmail.com>
2015-03-10 15:13:07 -0600

    Minor style fixes

--- lib/big_brother/runner.rb
+++ lib/big_brother/runner.rb
@@ -1,6 +1,5 @@
 class BigBrother::Runner
-
-  def self.run(cmd_line_args, options)
+  def self.run(_cmd_line_args, options)
     if options[:version]
       print_version
     else
@@ -15,5 +14,4 @@ class BigBrother::Runner
   def self.do_other_stuff
     puts "Big bro runner template"
   end
-
 end
